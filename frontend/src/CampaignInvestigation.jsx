ff344e98528f34a813b1e41b5910d6653963097c
Boniface Maburi <bmaburi@gmail.com>
2026-09-03 21:41:45 +0300

    Fix campaign investigation evidence graph axios import

--- frontend/src/CampaignInvestigation.jsx
+++ frontend/src/CampaignInvestigation.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useMemo, useState } from "react";
 import "./CampaignInvestigation.css";
 
+import axios from "axios";
 const API_BASE =
   import.meta.env.VITE_API_BASE || "http://127.0.0.1:8000";
 
